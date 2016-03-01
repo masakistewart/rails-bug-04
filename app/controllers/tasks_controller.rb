92a476b2cc21ed2476f440996664a328def99dfc
Cairo Stewart <stewartcairo@gmail.com>
2016-03-01 12:50:09 -0800

    hello

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-
   def new
     @task_list = TaskList.find(params[:task_list_id])
     @task = Task.new
@@ -8,6 +7,7 @@ class TasksController < ApplicationController
   def create
     task_params = params.require(:task).permit(:description, :due_date).merge(task_list_id: params[:task_list_id])
     @task = Task.new(task_params)
+    @task_list = TaskList.find(params[:task_list_id])
     if @task.save
       redirect_to root_path, notice: "Task was created successfully!"
     else
